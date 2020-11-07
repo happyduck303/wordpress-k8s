4ae091cd45560cef025bff9912ff5f9366530a46
M. Taufik Nelas <fickysyahreza.fs@gmail.com>
2020-11-07 11:30:16 +0700

    Delete production-deploy.sh

--- production-deploy.sh
+++
@@ -1,7 +0,0 @@
-kubectl create -f pod.yaml -n production
-kubectl create -f deployment.yaml -n production
-kubectl apply -f deployment.yaml -n production
-kubectl create -f service.yaml -n production
-kubectl apply -f service.yaml -n production
-kubectl create -f wordpress.yaml -n production
-
